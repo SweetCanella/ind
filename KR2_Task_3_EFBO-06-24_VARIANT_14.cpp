f74510ae00cc845fb2a350b7cf9d489a1011c9a6
Gesha <baramasya@gmail.com>
2024-10-19 09:43:41 +0300

    .

---
+++ KR2_Task_3_EFBO-06-24_VARIANT_14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+float f(float a, float b){
+    return a*b;
+}
+
+int main(){
+    float a,b;
+    cin>>a>>b;
+    cout<<f(a,b);
+}
+
+//Group: EFBO-06-24
+//Student: Kalchenko Evgeniya
